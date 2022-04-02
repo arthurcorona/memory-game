34fe950f200617ae6b90845ee22bd2b944a504bd
Arthur Corona <coronaggp@gmail.com>
2022-04-02 16:27:33 -0300

    erro no flip

---
+++ script/script.js
@@ -0,0 +1,120 @@
+const FRONT = "cardf"
+const BACK = "cardb"
+const CARD = "card"
+const ICON = "icon"
+
+let techs = [
+    'bootstrap',
+    'css',
+    'electron',
+    'firebase',
+    'html',
+    'javascript',
+    'jquery',
+    'mongo',
+    'node',
+    'react'
+]
+
+    let cards = null
+
+    startGame()
+
+    function startGame() {
+        cards = createCards(techs)
+        shuffleCards(cards)
+   
+        initializeCards(cards) 
+    }
+
+    function initializeCards(cards) {
+        let gameBoard = document.getElementById("gameBoard")
+        
+        cards.forEach(card => {
+            
+            let cardElement = document.createElement('div')
+            cardElement.id = card.id
+            cardElement.classList.add (CARD)
+            cardElement.dataset.icon = card.icon
+
+            createCardContent(card, cardElement)
+
+            cardElement.addEventListener('click', flipCard)
+            gameBoard.appendChild(cardElement)
+
+        })
+    }
+
+    function createCardContent(card, cardElement) {
+
+        createCardFace(FRONT, card, cardElement) 
+        createCardFace(BACK, card, cardElement) 
+
+    }
+
+    function createCardFace(face, card, element) {
+
+        let cardElementFace = document.createElement('div')
+        cardElementFace.classList.add('face')
+        if(face == FRONT) {
+            let iconElement = document.createElement('img')
+            iconElement.classList.add(ICON)
+            iconElement.src = "./assets/" + card.icon + ".png"
+            cardElementFace.appendChild(iconElement)
+        }
+        else {
+            cardElementFace.innerHTML = "&lt/&gt"
+        }
+
+        element.appendChild(cardElementFace)
+
+    }
+
+    //embaralhando cartas
+    function shuffleCards(cards) {
+        let currentIndex = cards.length
+        let randomIndex = 0
+
+        while (currentIndex != 0) {
+            
+            randomIndex = Math.floor(Math.random() * currentIndex)
+            currentIndex--
+
+            [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
+        }
+    }
+
+    //criando cartas
+    createCards(techs)
+    function createCards(techs) {
+        let cards = []
+
+        techs.forEach((tech) =>   {
+            cards.push(createPair(tech))
+        })
+    
+        return cards.flatMap(pair => pair)
+    }
+    
+
+    function createPair(tech) {
+        return [{
+            id: createId(tech),
+            icon: tech,
+            flipped: false
+        }, {
+            id: createId(tech),
+            icon: tech,
+            flipped: false
+        }  ]
+    }
+
+    function createId(tech) {
+        return tech + parseInt(Math.random() * 1000)
+    }
+
+    function flipCard() {
+
+        this.classList.add("flip")
+
+    }
